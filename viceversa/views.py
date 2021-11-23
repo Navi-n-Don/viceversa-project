2c255d49ea33306d7c2be1666ff76542ee438902
Navi-n-Don <navi-n-don@ukr.net>
2021-11-23 18:05:17 +0200

    Word count

--- viceversa/views.py
+++ viceversa/views.py
@@ -6,5 +6,9 @@ def home_page(request):
 
 def reverse(request):
     user_text = request.GET['reverse']
+    wordcountlist = []
+    for i in user_text.split():
+        wordcountlist.append(i.strip('!,. -_:;?'))
+    wordcount = len(wordcountlist)
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text, 'wordcount': wordcount})
